6ff624def2041153e66eacd24563f422ebdcc7da
Sebastian Waisbrot <seppo0010@gmail.com>
2015-04-10 15:22:53 -0700

    Basic print server

--- src/networking.rs
+++ src/networking.rs
@@ -1,10 +1,34 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use std::str::from_utf8;
 use std::thread;
+
+pub struct Client {
+    pub stream: TcpStream
+}
 
 pub struct Server {
     pub ip: String,
     pub port: i32,
-    pub clients: Vec<TcpStream>
+}
+
+impl Client {
+    pub fn new(stream: TcpStream) -> Client {
+        return Client {
+            stream: stream
+        }
+    }
+
+    pub fn read(&mut self) {
+        let mut buffer = [0u8; 512];
+        loop {
+            let usize = self.stream.read(&mut buffer).unwrap();
+            if usize == 0 {
+                break;
+            }
+            println!("{} {}", from_utf8(&buffer).unwrap(), usize);
+        };
+    }
 }
 
 impl Server {
@@ -12,25 +36,28 @@ impl Server {
         return Server {
             ip: ip.to_string(),
             port: *port,
-            clients: Vec::new()
         }
     }
 
     pub fn handle_client(&mut self, stream: TcpStream) {
-        self.clients.push(stream);
-        println!("Hello, client!");
+        println!("Client connected");
+        thread::spawn(move || {
+            let mut client = Client::new(stream);
+            client.read();
+        });
     }
 
     pub fn start(&mut self) {
         let addr: String = format!("{}:{}", self.ip, self.port);
         let listener = TcpListener::bind(&*addr).unwrap();
+        println!("Listening to new connections on {}", addr);
         for stream in listener.incoming() {
             match stream {
                 Ok(stream) => {
                     // connection succeeded
                     self.handle_client(stream)
                 }
-                Err(e) => { /* connection failed */ }
+                Err(e) => { println!("{}", e); }
             }
         }
         // close the socket server
--- src/rsedis.rs
+++ src/rsedis.rs
@@ -3,7 +3,6 @@ use self::networking::Server;
 pub mod networking;
 
 fn main() {
-    println!("Hello, world!");
     let port: i32 = 6379;
     let mut server = Server::new("127.0.0.1", &port);
     server.start();
