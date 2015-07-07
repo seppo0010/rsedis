2b7857a5cced6982f7b355a6040749d7c1db75bf
Sebastian Waisbrot <seppo0010@gmail.com>
2015-07-07 16:52:34 -0300

    Document response functions

--- response/src/lib.rs
+++ response/src/lib.rs
@@ -1,19 +1,29 @@
 use std::sync::mpsc::Receiver;
 use std::fmt::{Debug, Formatter, Error};
 
-#[derive(PartialEq)]
-#[derive(Debug)]
+/// A command response to send to a client
+#[derive(PartialEq, Debug)]
 pub enum Response {
+    /// No data
     Nil,
+    /// A number
     Integer(i64),
+    /// Binary data
     Data(Vec<u8>),
+    /// A simple error string
     Error(String),
+    /// A simple status string
     Status(String),
+    /// An array of responses that may mix different types
     Array(Vec<Response>),
 }
 
+/// No response was issued
 pub enum ResponseError {
+    /// The command generated no response
     NoReply,
+    /// The command generated no response yet, but if the receiver reads a `true`,
+    /// the command can be retried.
     Wait(Receiver<bool>),
 }
 
@@ -27,6 +37,7 @@ impl Debug for ResponseError {
 }
 
 impl Response {
+    /// Serializes the response into an array of bytes using Redis protocol.
     pub fn as_bytes(&self) -> Vec<u8> {
         return match *self {
             Response::Nil => b"$-1\r\n".to_vec(),
@@ -40,6 +51,7 @@ impl Response {
         }
     }
 
+    /// Returns true if and only if the response is an error.
     pub fn is_error(&self) -> bool {
         match *self {
             Response::Error(_) => true,
