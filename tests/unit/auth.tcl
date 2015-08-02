3a51d15769097869e7233f5e1f43f7837bdd0c43
Sebastian Waisbrot <seppo0010@gmail.com>
2015-08-02 14:13:38 -0700

    Restore auth tests

--- tests/unit/auth.tcl
+++ tests/unit/auth.tcl
@@ -1,26 +1,26 @@
 start_server {tags {"auth"}} {
-    xtest {AUTH fails if there is no password configured server side} {
+    test {AUTH fails if there is no password configured server side} {
         catch {r auth foo} err
         set _ $err
     } {ERR*no password*}
 }
 
 start_server {tags {"auth"} overrides {requirepass foobar}} {
-    xtest {AUTH fails when a wrong password is given} {
+    test {AUTH fails when a wrong password is given} {
         catch {r auth wrong!} err
         set _ $err
     } {ERR*invalid password}
 
-    xtest {Arbitrary command gives an error when AUTH is required} {
+    test {Arbitrary command gives an error when AUTH is required} {
         catch {r set foo bar} err
         set _ $err
     } {NOAUTH*}
 
-    xtest {AUTH succeeds when the right password is given} {
+    test {AUTH succeeds when the right password is given} {
         r auth foobar
     } {OK}
 
-    xtest {Once AUTH succeeded we can actually send commands to the server} {
+    test {Once AUTH succeeded we can actually send commands to the server} {
         r set foo 100
         r incr foo
     } {101}
