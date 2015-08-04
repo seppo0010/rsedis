93d4e95b47cf825f817821f3a8e357682969db68
Sebastian Waisbrot <seppo0010@gmail.com>
2015-08-04 12:57:23 -0700

    Restore some keyspace tests

--- tests/unit/keyspace.tcl
+++ tests/unit/keyspace.tcl
@@ -1,34 +1,34 @@
 start_server {tags {"keyspace"}} {
-    xtest {DEL against a single item} {
+    test {DEL against a single item} {
         r set x foo
         assert {[r get x] eq "foo"}
         r del x
         r get x
     } {}
 
-    xtest {Vararg DEL} {
+    test {Vararg DEL} {
         r set foo1 a
         r set foo2 b
         r set foo3 c
         list [r del foo1 foo2 foo3 foo4] [r mget foo1 foo2 foo3]
     } {3 {{} {} {}}}
 
-    xtest {KEYS with pattern} {
+    test {KEYS with pattern} {
         foreach key {key_x key_y key_z foo_a foo_b foo_c} {
             r set $key hello
         }
         lsort [r keys foo*]
     } {foo_a foo_b foo_c}
 
-    xtest {KEYS to get all keys} {
+    test {KEYS to get all keys} {
         lsort [r keys *]
     } {foo_a foo_b foo_c key_x key_y key_z}
 
-    xtest {DBSIZE} {
+    test {DBSIZE} {
         r dbsize
     } {6}
 
-    xtest {DEL all keys} {
+    test {DEL all keys} {
         foreach key [r keys *] {r del $key}
         r dbsize
     } {0}
@@ -41,7 +41,7 @@ start_server {tags {"keyspace"}} {
         r debug set-active-expire 1
     }
 
-    xtest {EXISTS} {
+    test {EXISTS} {
         set res {}
         r set newkey test
         append res [r exists newkey]
@@ -49,7 +49,7 @@ start_server {tags {"keyspace"}} {
         append res [r exists newkey]
     } {10}
 
-    xtest {Zero length value in key. SET/GET/EXISTS} {
+    test {Zero length value in key. SET/GET/EXISTS} {
         r set emptykey {}
         set res [r get emptykey]
         append res [r exists emptykey]
@@ -57,9 +57,9 @@ start_server {tags {"keyspace"}} {
         append res [r exists emptykey]
     } {10}
 
-    xtest {Commands pipelining} {
+    test {Commands pipelining} {
         set fd [r channel]
-        puts -nonewline $fd "SET k1 xyzk\r\nGET k1\r\nPING\r\n"
+        puts -nonewline $fd "*3\r\n\$3\r\nSET\r\n\$2\r\nk1\r\n\$4\r\nxyzk\r\n*2\r\n\$3\r\nGET\r\n\$2\r\nk1\r\n*1\r\n\$4\r\nPING\r\n"
         flush $fd
         set res {}
         append res [string match OK* [r read]]
@@ -68,7 +68,7 @@ start_server {tags {"keyspace"}} {
         format $res
     } {1xyzk1}
 
-    xtest {Non existing command} {
+    test {Non existing command} {
         catch {r foobaredcommand} err
         string match ERR* $err
     } {1}
@@ -152,14 +152,14 @@ start_server {tags {"keyspace"}} {
         r ttl mykey2
     } {-1}
 
-    xtest {DEL all keys again (DB 0)} {
+    test {DEL all keys again (DB 0)} {
         foreach key [r keys *] {
             r del $key
         }
         r dbsize
     } {0}
 
-    xtest {DEL all keys again (DB 1)} {
+    test {DEL all keys again (DB 1)} {
         r select 10
         foreach key [r keys *] {
             r del $key
@@ -193,7 +193,7 @@ start_server {tags {"keyspace"}} {
         set e
     } {*ERR*index out of range}
 
-    xtest {SET/GET keys in different DBs} {
+    test {SET/GET keys in different DBs} {
         r set a hello
         r set b world
         r select 10
@@ -240,7 +240,7 @@ start_server {tags {"keyspace"}} {
         r randomkey
     } {}
 
-    xtest {KEYS * two times with long key, Github issue #1208} {
+    test {KEYS * two times with long key, Github issue #1208} {
         r flushdb
         r set dlskeriewrioeuwqoirueioqwrueoqwrueqw test
         r keys *
